428d66cd859f9f9ce068abbb22b83500c5fe19db
YUMI NARITA <narita.yumi@canon-its.co.jp>
2016-07-03 06:42:03 +0000

    課題　アイテムの実装
    ・アイテムのタイトル・画像が適切に表示されていること
    ・アイテムの楽天市場へのリンクが貼られていること
    ・アイテムを「Have」「Want」したユーザーのアイコン一覧をそれぞれ表示すること
    ・表示されたユーザーアイコンには各ユーザーページのリンクが貼ってあること
    ・metaタグが適切に設定されていること

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @have_users = @item.have_users
+    @want_users = @item.want_users
   end
 
   private
